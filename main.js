8a241da8d0e12496f2ea5c9f0fd1a53a61ecce46
Ruslan <ruslanvs@yahoo.com>
2017-12-21 17:36:21 -0800

    Advanced and simplified solution

--- main.js
+++ main.js
@@ -1,49 +1,6 @@
 $(document).ready(function() {
-
-    
     $( 'img' ).click( function () {
         console.log ('click!');
-        
-        $( 'img' ).each( function ( i, img ) {
-            $( this ).attr( 'id', "img" + i );
-            console.log ($( this ).attr( 'id' ));
-        })
-
-
-        
-        /* THIS WAS AN INITIAL APPROACH WITH MANUALLY SET ID'S AND MANUALLY SET WIDTH
-        var currentId;
-        var arr=[];
-        var nextId;
-        var margin;
-
-        currentId = $( this ).attr( 'id' );
-        console.log (currentId);
-
-        arr = currentId.split('img');
-        console.log (arr);
-
-        nextId = 'img' + (Number (arr [1]) + 1);
-        console.log ( nextId );
-
-        check = $( '#' + nextId ).attr( 'id' );
-        console.log ( check );
-
-        margin = $( '#' + nextId ).css('margin-left');
-        mnrarr = margin.split('px');
-        console.log (mnrarr);
-        mrn = Number ( mnrarr[0] )
-
-        console.log (margin, mrn  );
-
-        margin = mrn + 160;
-        console.log (margin);
-
-        $( '#' + nextId ).css( 'margin-left', margin )
-        console.log ($( '#' + nextId ).css('margin-left' )); */
-
-        $( this ).hide ();
-
+        $( this ).css ('visibility' , 'hidden');
     })
-
 })
